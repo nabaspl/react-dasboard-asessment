d5915705b74280e5471bb60e7f5f8cbe347a293e
nabaspl <nabas@piktorlabs.com>
2021-10-08 21:21:05 +0530

    modal issue fixed

--- src/pages/safe/index.js
+++ src/pages/safe/index.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import MainWrapper from '../../components/main/wrapper/mainWrapper';
 import SideNav from '../../components/sideNav/sideNav';
 import MainContents from '../../components/mainContent/mainContent';
-import Modal from '../../components/modal/modal';
-import SafeForm from './components/form/safeForm';
 import SideNavTop from '../../components/sideNavTop/sideNavTop';
 import SideNavBody from '../../components/sideNavBody/sideNavBody';
 import './style.css';
 import {SideNavTopContent,SideNavBodyContent} from './components/sideNavContents/sideNavContent'
 
+import {useSelector} from 'react-redux'
 
-class Safe extends React.Component {
+
+function Safe(){
+
     
-    render() { 
-        let items=[{safeName:"test",safeOwner:"tester",safeType:'personal',safeDescription:'test'},{safeName:"test",safeOwner:"tester",safeType:'personal',safeDescription:'test'}];
+
+        
         return  <MainWrapper>
                     <SideNav>
                         <SideNavTop>
@@ -26,17 +27,13 @@ class Safe extends React.Component {
         
                     </SideNav>
                     <MainContents>
-
+                       
                     </MainContents>
-                    <Modal>
-                        <SafeForm/>
-                    </Modal>
+                    
                 </MainWrapper>;
-    }
-    
-
-    
+   
 }
+
 
 
 export default Safe;
