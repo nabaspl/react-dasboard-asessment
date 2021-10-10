510fa7f2c9931fd43f6350f8fa5604a72ae90ca3
nabaspl <nabas@piktorlabs.com>
2021-10-10 13:46:35 +0530

    query params

--- src/pages/serviceAccounts/index.js
+++ src/pages/serviceAccounts/index.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import MainWrapper from '../../components/main/wrapper/mainWrapper';
 import SideNav from '../../components/sideNav/sideNav';
 import MainContents from '../../components/mainContent/mainContent';
-
-class ServiceAccount extends React.Component {
-    render() { 
-        return  <MainWrapper>
-                    <SideNav></SideNav>
-                    <MainContents></MainContents>
-                </MainWrapper>;
+import {useLocation} from "react-router-dom";
+function ServiceAccount(){
+    const useQuery =()=> {
+        return new URLSearchParams(useLocation().search);
     }
+    let query = useQuery();
+    let pageName=query.get("name");
+    
+        return  <MainWrapper>
+                    <SideNav>
+                        <p>{pageName}</p>
+                    </SideNav>
+                    <MainContents>
+                    </MainContents>
+                </MainWrapper>;
+    
 }
  
 export default ServiceAccount;
