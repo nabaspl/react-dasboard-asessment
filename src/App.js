fc779b2f0a1eb78457e7f8c3fcc71c19fdb3b38d
nabaspl <nabas@piktorlabs.com>
2021-10-10 13:54:35 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
-import { Fragment } from 'react';
-import './App.css';
-import Header from './components/header/Header';
-import Main from './components/main/main';
-import Safe from './pages/safe';
-import Vault from './pages/vault';
-import IamService from './pages/iamServiceAccounts';
-import ServiceAccount from './pages/serviceAccounts';
-import User from './pages/user';
-import Documentation from './pages/documentation';
-import Azure from './pages/azureActiveDirectory';
-import { BrowserRouter as Router, Switch,  Route,  Link,useLocation} from "react-router-dom";
-import { menus } from './menus';
+import { Fragment } from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Main from "./components/main/main";
+import Safe from "./pages/safe";
+import Vault from "./pages/vault";
+import IamService from "./pages/iamServiceAccounts";
+import ServiceAccount from "./pages/serviceAccounts";
+import User from "./pages/user";
+import Documentation from "./pages/documentation";
+import Azure from "./pages/azureActiveDirectory";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { menus } from "./menus";
 
 function App() {
   return (
     <Router>
-      <Header menuLists={menus}/>
+      <Header menuLists={menus} />
       <Main>
-      <Switch>
-          <Route path="/safe">
-            <Safe />
-          </Route>
-          <Route path="/vault">
-            <Vault />
-          </Route>
-          <Route path="/service">
-            <ServiceAccount />
-          </Route>
-          <Route path="/iam">
-            <IamService />
-          </Route>
-          <Route path="/azure">
-            <Azure />
-          </Route>
-          <Route path="/documentation">
-            <Documentation />
-          </Route>
-          <Route path="/user">
-            <User />
-          </Route>
-      </Switch>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return <Redirect to="/safe" />;
+            }}
+          />
+          <Route path="/safe" component={Safe} />
+          <Route path="/vault" component={Vault} />
+          <Route path="/service" component={ServiceAccount} />
+          <Route path="/iam" component={IamService} />
+          <Route path="/azure" component={Azure} />
+          <Route path="/documentation" component={Documentation} />
+          <Route path="/user" component={User} />
+        </Switch>
       </Main>
     </Router>
   );
