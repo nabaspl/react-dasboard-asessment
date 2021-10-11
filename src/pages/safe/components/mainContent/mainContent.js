69b828a51618bb17ddd44c5451f294b6ff19ba3a
nabaspl <nabas@piktorlabs.com>
2021-10-11 16:36:12 +0530

    secret modal closing issue fixed

--- src/pages/safe/components/mainContent/mainContent.js
+++ src/pages/safe/components/mainContent/mainContent.js
@@ -62,7 +62,7 @@ export function MainContentBody(props) {
         }
          
       </div>
-      {modalShow && (<Modal><SecretForm safeId={props.safe.safeId} secret=""  handleOnSubmit={handleSubmit}/></Modal>)}
+      {modalShow && (<Modal><SecretForm safeId={props.safe.safeId} secret=""  handleOnSubmit={handleSubmit} CloseModal={handleCloseModal}/></Modal>)}
     </div>
   );
 }
