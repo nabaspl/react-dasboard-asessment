bf2a319a1d013c685552faa9974a1f505c9cd966
nabaspl <nabas@piktorlabs.com>
2021-10-13 10:16:09 +0530

    fixed issues

--- src/pages/safe/components/form/secretForm.js
+++ src/pages/safe/components/form/secretForm.js
@@ -34,6 +34,7 @@ return <form onSubmit={handleSubmit}>
         <h1 className="form-name">Add Folder</h1>
         <Input type="hidden" name="safeId" id="safeId" value={values.safeId||0} onChange={handleInputChange}/>
         <Input type="text" id="secret" name="secret" value={values.secret||''} onChange={handleInputChange} label="Folder Name" placeHolder="enter foldername"/>
+        <p className="info">Please enter a minimum of 3 characters lowercase alphabets numbebr and underscores only. </p>
         <div className="button-group" style={{marginTop:"3.938rem"}}>
             <span className="cancel-btn" onClick={props.CloseModal}>Cancel</span>
             <Button>+ Create</Button>
